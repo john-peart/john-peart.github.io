12e0d670f26e2341168abb72301b90638a86ddb1
john-peart <john.peart@gmail.com>
2020-06-22 09:41:51 -0400

    Initial Commit

---
+++ scripts/pokedex.js
@@ -0,0 +1,203 @@
+//WRAPPER FOR ADDING PROMSIES TO IndexedDB API - https://github.com/jakearchibald/idb
+import { openDB, deleteDB, wrap, unwrap } from './idb.js';
+
+export default function Pokedex(container) {
+  this._dbPromise = openDatabase();
+  //this._registerServiceWorker();
+}
+
+const pokemonURL = "https://pokeapi.co/api/v2/pokemon?limit=5000";
+const pokemonStoreName = "Pokemon";
+const speciesURL = "https://pokeapi.co/api/v2/pokemon-species?limit=1000";
+const speciesStoreName = "Species";
+const generationURL = "https://pokeapi.co/api/v2/generation?limit=5000";
+const generationStoreName = "Generations";
+const typeURL = "https://pokeapi.co/api/v2/type?limit=100";
+const typeStoreName = "Types"
+
+function openDatabase() {
+  // If the browser doesn't support service worker,
+  // we don't care about having a database
+  if (!navigator.serviceWorker) {
+    return Promise.resolve();
+  }
+
+  var dbPromise = openDB('pokedex', 1, {
+    upgrade(db, oldVersion, newVersion, transaction) {
+
+      var pokemonStore = db.createObjectStore(pokemonStoreName, { keyPath: 'id' });
+      console.log("Created Pokemon store");
+      pokemonStore.createIndex('name', 'name');
+      pokemonStore.createIndex('order', 'order');
+      console.log("Created index on Pokemon object store");
+
+      var speciesStore = db.createObjectStore(speciesStoreName, { keyPath: 'name' });
+      console.log("Created Species store");
+      speciesStore.createIndex("id", "id");
+      speciesStore.createIndex("order", "order");
+      console.log("Created index on Sepecies object store");
+
+      var generationStore = db.createObjectStore(generationStoreName, { keyPath: 'name' });
+      console.log("Created Generations store");
+      generationStore.createIndex("id", "id");
+      console.log("Created index on Generations object store");
+
+      var generationStore = db.createObjectStore(typeStoreName, { keyPath: 'name' });
+      console.log("Created Type store");
+      generationStore.createIndex("name", "name");
+      console.log("Created index on Type object store");
+    }
+  });
+  return dbPromise;
+}
+
+function HectogramToPounds(weigth) {
+  const hectogramsToPounds = 0.220462
+  return Math.round(weigth * hectogramsToPounds);
+}
+
+function DecimeterToFeetAndInches(height) {
+  const decimeterToFeet = 0.3280839895;
+  var valueInFeet = height * decimeterToFeet;
+  var feet = Math.floor(valueInFeet);
+  var inches = Math.round((valueInFeet - feet) * 12);
+  return `${feet}' ${inches}"`;
+}
+
+function PokeCardHtml(character, species, generation) {
+  var types = character.types.map((type) => `<span class="capitalize badge badge-${type.type.name} mr-1 px-3 py-1">${type.type.name}</span>`).join(``);
+  var img = character.sprites['front_default'];
+  if (!img) {
+    img = "/images/image-placeholder.png"
+  }
+  
+  var englishText = species.flavor_text_entries.filter(entry => entry.language.name === "en")[0].flavor_text;
+  if(!englishText)
+  {
+    englishText = species.flavor_text_entries[0].flavor_text;
+  }
+
+  var html = ` 
+    <div class="card shadow mx-auto my-1 bg-${character.types[0].type.name}" data-id="${character.id}">
+      <div class="bg-${character.types[0].type.name}-dark rounded-top">
+        <img class="card-img-top mx-auto d-block pokemon"  src="${img}" alt="Card image cap" />
+      </div>          
+      <div class="card-body bg-${character.types[0].type.name}-light rounded-bottom">
+          <div class="pokemon-types">
+            ${types}
+          </div>
+          <h4 class="card-title text-center capitalize mb-1">${character.name}</h4>              
+          <p class="card-text mb-1"><small>${englishText}</small></p>
+          <p class="card-text mb-1"><small>
+            ${species.generation.name}<br/>
+            Height: ${DecimeterToFeetAndInches(character.height)}<br/>
+            Weight: ${HectogramToPounds(character.weight)}lbs<br>
+            Region: ${generation.main_region.name}
+            </small>
+          </p>
+      </div>
+    </div>
+  `;
+  return html;
+}
+
+function LoadJSON(url) {
+  return fetch(url)
+    .then(response => response.json())
+}
+
+function LoadApiData(db,storeName,url, forceReload){
+  console.log("Loading " + storeName);
+
+  var results;
+  return LoadJSON(url)
+    .then(async function (apiData) {
+
+      var tx = db.transaction(storeName);
+      var dbStore = tx.objectStore(storeName);
+      let dbData = await dbStore.getAll();
+
+      //see if the number of results in index DB matches the number returned by the API
+      if (forceReload || !dbData || dbData.length < apiData.results.length) {
+        //update from the API because we don't have any OR there are  entries we don't have
+        console.log("Updating IndexDB store " + storeName + " from " + url);
+        const promises = [];
+        apiData.results.forEach(item => {
+          promises.push(fetch(item.url).then((res) => res.json()));
+        });
+        return Promise.all(promises).then(function (results) {
+          var tx = db.transaction(storeName, "readwrite");
+          var store = tx.objectStore(storeName);
+          results.forEach(item => {
+            store.put(item);
+          });
+          return results;
+        });
+      }
+      else {
+        console.log("Using cached IndexDB data for store " + storeName);
+        return dbData;
+      }
+
+    })
+}
+
+async function BuildPokemonCards(db,pokemon){
+
+  var htmlArr = pokemon.map(async (character) => {
+    let tx = db.transaction(speciesStoreName);
+    var dbStore = tx.objectStore(speciesStoreName);
+    var species = await dbStore.get(character.species.name);
+
+    tx = db.transaction(generationStoreName);
+    dbStore = tx.objectStore(generationStoreName);              
+    var generation = await dbStore.get(species.generation.name)
+
+    return PokeCardHtml(character,species,generation)          
+  })
+  
+  return Promise.all(htmlArr)
+
+}
+
+function hideLoading() {
+  document.getElementById("loading").classList.add("d-none");
+  document.getElementById("pokemonContainer").classList.remove("d-none");
+}
+
+function init(containerName) {
+  var dbPromise = openDatabase();
+
+  dbPromise.then(function (db) {
+    if (!db) {
+      console.log("DB not defined");
+    }
+    else {
+      db.resolve;
+      console.log(db);
+    }
+
+    var pokemonData;
+    LoadApiData(db,pokemonStoreName,pokemonURL)
+      .then((pokemon) => {
+        pokemonData = pokemon;
+        return LoadApiData(db,speciesStoreName,speciesURL);
+      })
+      .then(() => {
+        return LoadApiData(db,generationStoreName,generationURL);
+      })
+      .then(() => {
+        return LoadApiData(db,typeStoreName,typeURL);
+      })
+      .then(() => {               
+        return BuildPokemonCards(db, pokemonData);
+      })
+      .then((html)=>{
+        document.getElementById(containerName).innerHTML = html;
+        hideLoading();
+      });
+  })
+}
+
+init("pokemonCardDeck");
+
---
+++ service-worker.js
@@ -0,0 +1,57 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+workbox.setConfig({
+  debug: true
+});
+
+//precache local resources and CDN resources
+workbox.precaching.precacheAndRoute([
+  {url: 'pokedex.html', revision: null},
+  {url: 'manifest.json', revision: null},
+  {url: '/scripts/pokedex.js', revision: null},
+  {url: '/scripts/idb.js', revision: null},
+  {url: '/scripts/wrap-idb-value.js', revision: null},
+  {url: '/styles/app.css', revision: null},
+  {url: '/images/pokemon_logo_1024.png', revision: null},
+  {url: '/images/simple_pokeball.gif', revision: null},
+  {url: '/images/icon-128x128.png', revision: null},
+  {url: '/images/icon-192x192.png', revision: null},
+  {url: '/images/icon-256x256.png', revision: null},
+  {url: '/images/icon-384x384.png', revision: null},
+  {url: '/images/favicon-16x16.png', revision: null},
+  {url: '/images/favicon-32x32.png', revision: null},
+  {url: '/images/image-placeholder.png', revision: null},
+  {url: 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', revision: null},
+  {url: 'https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js', revision: null}
+]);
+
+// Demonstrates using default cache
+workbox.routing.registerRoute(
+    new RegExp('.*\\.(?:js)'),
+    new workbox.StaleWhileRevalidate(),
+);
+
+// Demonstrates a custom cache name for a route.
+workbox.routing.registerRoute(
+    new RegExp('.*\\.(?:png|jpg|jpeg|svg|gif)'),
+    new workbox.strategies.CacheFirst({
+      cacheName: 'image-cache',
+      plugins: [
+        new workbox.cacheableResponse.CacheableResponsePlugin({
+          statuses: [0, 200]
+        })
+      ]
+    })
+);
+
+workbox.routing.registerRoute(
+  ({url}) => url.origin === 'https://pokeapi.co',
+  new workbox.strategies.CacheFirst({
+    cacheName: 'api-cache',
+    plugins: [
+      new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [0, 200],
+      })
+    ]
+  })
+);
